976ccd1e94673349edf9c13463cc23bc372812c1
xPetersue <xpetersue@gmail.com>
2023-08-16 21:47:04 +0100

    Create RedefinitionLightingChatGPT.py

---
+++ RedefinitionLightingChatGPT.py
@@ -0,0 +1,17 @@
+import openai
+openai.api_key = 'Your_Secret_Key'
+
+messages = [ {"role": "system", "content": 
+              "I am your Smart Home Lighting Controller"} ]
+while True:
+    message = input("User : ")
+    if message:
+        messages.append(
+            {"role": "user", "content": message},
+        )
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages
+        )
+    reply = chat.choices[0].message.content
+    print(f"ChatGPT: {reply}")
+    messages.append({"role": "assistant", "content": reply})
